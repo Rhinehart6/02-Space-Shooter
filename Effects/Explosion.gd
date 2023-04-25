815af0294728ff431c8da624688af89ccec5100e
Rhinehart6 <abbrhine@iu.edu>
2023-04-24 22:10:57 -0400

    Finished the Project

---
+++ Effects/Explosion.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite
+
+
+func _ready():
+	play("default")
+	var Explosion_Sound = get_node_or_null("/root/Game/Explosion_Sound")
+	if Explosion_Sound != null:
+		Explosion_Sound.play()
+
+
+func _on_Explosion_animation_finished():
+	queue_free()
--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -4,17 +4,27 @@ var velocity = Vector2.ZERO
 var speed = 500.0
 var damage = 1
 
+onready var Explosion = load("res://Effects/Explosion.tscn")
+var Effects = null
+
 func _ready():
 	velocity = Vector2(0,-speed).rotated(self.rotation)
 
 func _physics_process(_delta):
 	velocity = move_and_slide(velocity, Vector2.ZERO)
-	position.x = wrapf(position.x, 0, 1024)
-	position.y = wrapf(position.y, 0, 600)
+	position.x = wrapf(position.x, 0, Global.VP.x)
+	position.y = wrapf(position.y, 0, Global.VP.y)
 
 
 
 func _on_Area2D_body_entered(body):
+	if body.has_method("damage"):
+		body.damage(damage)
+	Effects = get_node_or_null("/root/Game/Effects")
+	if Effects != null:
+		var explosion = Explosion.instance()
+		Effects.add_child(explosion)
+		explosion.global_position = global_position
 	queue_free()
 
 
